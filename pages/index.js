ba64007b42ae16501e5430af21e3cf2460d556ac
Enes Erden <admin@eneserden.com>
2022-04-23 02:21:21 +0300

    Sanity Connected Frontend

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
 import react from "react";
 import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
-const Home = () => {
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>speakers</p>
       </div>
       <div className="products-container">
-        {["product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       FOOTER
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = "*[_type == 'product']";
+  const products = await client.fetch(query);
+
+  const bannerQuery = "*[_type == 'banner']";
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
 };
 
 export default Home;
