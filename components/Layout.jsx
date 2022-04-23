0c78b9efd37950b63940b32b21f8529165f79a1f
Enes Erden <admin@eneserden.com>
2022-04-23 03:07:31 +0300

    Layout added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Head from "next/head";
 
-const Layout = () => {
-  return <div>Layout</div>;
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <div className="layout">
+        <Head>
+          <title>NextJS Store</title>
+        </Head>
+        <header>
+          <Navbar />
+        </header>
+        <main className="main-container">{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
